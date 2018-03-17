64f07198beb026d5df43d7a61253a6f6467f5602
eric.kemmis <eric.kemmis@icloud.com>
2018-03-17 16:08:54 -0500

    Completed trie statement, added jquery and a text input that checks for word as input changes. Stuck on trieing to find all the Stuck on Trie.prototype.getWords. Recursion or looping while not able to refer to a parent object...

--- js/main.js
+++ js/main.js
@@ -42,9 +42,63 @@ Trie.prototype.checkWord = function(word) {
   return curs.isWord ? true : false;
 }
 
+Trie.prototype.getWords = function(letters, node, word, ) {
 
-var trie = new Trie();
-trie.loadWord("amber");
-trie.loadWord("ambiant");
+  //if there is no wat to find the parent looks like recursion is the only way :(
+  //while loop
+  var end = false,
+      node = this.root,
+      pos = 0,
+      word = '',
+      childNodes = Object.keys(node.children),
+      wordList = [];
 
-console.log(trie);
+  while(!end) {
+    if(letters.indexOf(node.child[pos]) > -1) {
+      word += childNodes[pos];
+
+
+    }
+  }
+}
+
+
+
+
+$(document).ready(function(){
+
+  //--------Setup Trie statement----------------///
+  var trie = new Trie(),
+      JSON_words = [word2, word3, word4, word5, word6, word7,
+               word8, word9, word10, word11, word12],
+      correctWords = [];
+
+
+  //build Trie using dictionary of words
+  JSON_words.forEach(function(wordList){
+    //add to trie statement
+    for(var i = 0, len = wordList.length; i < len; i++) {
+      trie.loadWord(wordList[i].word);
+    }
+    //dump JSON from memory
+    wordList = null;
+  });
+
+  //do not refresh on submit
+  $('#word-form').submit(function(e) {
+    e.preventDefault();
+  });
+
+
+  $('.word-input').on('input', function(){
+    var word = $(this).val();
+    if(trie.checkWord(word) && correctWords.indexOf > -1) {
+      console.log(word + 'isword');
+
+    }else{
+      console.log('not a sword');
+    }
+  });
+  letters = ['a','g','r','c','b']
+  trie.getWords(letters);
+});
